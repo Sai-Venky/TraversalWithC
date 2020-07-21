e69ebf43407cd28c774f099b3f4ff520caf69fbc
R.Sai Venkatesh <venkysai.96@gmail.com>
2020-07-21 23:52:02 +0530

    Adding count-complete-tree-nodes solution

---
+++ leet-code/count-complete-tree-nodes.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/count-complete-tree-nodes
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int lastLevelCount = 0;
+    int totalLevel = 0;
+
+public:
+    
+    void countCompleteTreeNodes(TreeNode* root, int level) {
+        if(root == NULL) {
+            return;
+        }
+        
+        if(level > totalLevel) totalLevel = level;
+        
+        if(root->left == NULL && root->right == NULL && level == totalLevel) {
+            lastLevelCount ++;
+            return;
+        }
+        
+        countCompleteTreeNodes(root->left, level + 1);
+        
+        countCompleteTreeNodes(root->right, level + 1);
+    }
+    
+    int countNodes(TreeNode* root) {
+        int i = 0, count = 0;
+        countCompleteTreeNodes(root, 0);
+        
+        
+        while(i < totalLevel) {
+            count += pow(2, i);
+            i++;
+        }
+     
+        return count + lastLevelCount;
+    }
+};
