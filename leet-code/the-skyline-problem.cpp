f7e76d8f373366d1fc11b4f725ff7aa76ed673a8
R.Sai Venkatesh <v.ramesh@samsung.com>
2020-08-03 13:17:45 +0530

    Adding the-skyline-problem solution

---
+++ leet-code/the-skyline-problem.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/the-skyline-problem
+
+bool equalityCheck(vector<int> a, vector<int> b) 
+{ 
+    return a[1] == b[1];
+} 
+
+class Solution {
+public:
+
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+		multimap<int, int> coords;
+		for (auto building : buildings) {
+			coords.emplace(building[0], building[2]);
+			coords.emplace(building[1], -building[2]);
+		}
+
+		vector<vector<int>> results;
+		multiset<int> heights = { 0 };
+		map<int, int> corners;
+		for (auto p : coords) {
+			if (p.second > 0) {
+				heights.insert(p.second);
+			}
+			else {
+				heights.erase(heights.find(-p.second));
+			}
+			int cur_y = *heights.rbegin();
+			corners[p.first] = cur_y;
+		}
+
+        int current;
+        for(auto corner: corners) {
+            if(corner.second == current) continue;
+            results.push_back({corner.first ,corner.second});
+            current = corner.second;
+        }
+        return results;
+    }
+};
