624b537f40ff9f0334c8e25a5012b0796eabb5f6
R.Sai Venkatesh <venkysai.96@gmail.com>
2020-10-03 17:01:31 +0530

    Adding sum-of-root-to-leaf-binary-numbers Solution

---
+++ challenges/sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+        
+    int decimalValue(vector<int> path) {
+        int level = 0, decimalValue = 0;
+        for (auto i = path.crbegin(); i != path.crend(); ++i, level++) {
+            if(*i == 0)  continue;
+            decimalValue += pow(2, level);
+        }
+        return decimalValue;
+    }
+    
+    void sumRootToLeafs(TreeNode* root, vector<int> path) {
+        if(root == NULL) return;
+        path.push_back(root->val);
+        if(root->left == NULL && root->right == NULL) {
+            sum += decimalValue(path);
+            return;
+        }
+        sumRootToLeafs(root->left, path);
+        sumRootToLeafs(root->right, path);
+    }
+    
+    int sumRootToLeaf(TreeNode* root) {
+        vector <int> path;
+        sumRootToLeafs(root, path);
+        return sum;
+        
+    }
+};
