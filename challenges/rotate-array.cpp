5274f4fb32bea33b6d4591a729a45b83c674c574
R.Sai Venkatesh <venkysai.96@gmail.com>
2020-10-17 19:48:48 +0530

    Adding rotate-array solution

---
+++ challenges/rotate-array.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/rotate-array
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+    k = k % nums.size();
+    int count = 0;
+    for (int start = 0; count < nums.size(); start++) {
+      int current = start;
+      int prev = nums[start];
+      do {
+        int next = (current + k) % nums.size();
+        int temp = nums[next];
+        nums[next] = prev;
+        prev = temp;
+        current = next;
+        count++;
+      } while (start != current);
+    }
+  }
+};
