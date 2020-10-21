52781c34a46f76e1833621f4083e3463368f3c96
R.Sai Venkatesh <venkysai.96@gmail.com>
2020-10-21 09:01:18 +0530

    Adding move-zeros solution

---
+++ challenges/move-zeroes.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/move-zeroes
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0, i = 0;
+        for (; i < nums.size(); i ++)  {
+            if (nums[i] !=0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (; j < nums.size();)  {
+            nums[j++] = 0;
+        }
+    }
+};
