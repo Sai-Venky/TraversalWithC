692fe35c6188012a2259837aadcf1bb17f4cfa1d
Sai Venkatesh <venkysai.96@gmail.com>
2021-03-28 21:03:23 +0530

    Adding merge-two-sorted-lists solution

---
+++ challenges/merge-two-sorted-lists.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/merge-two-sorted-lists
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+      ListNode *l3= new ListNode;
+      ListNode *head=l3;
+      while(l1!=NULL && l2!=NULL) {
+        ListNode *temp = new ListNode;
+        if(l1->val>l2->val) {
+          temp->val = l2->val;
+          l2 = l2->next;
+        } else {
+          temp->val = l1->val;
+          l1 = l1->next;
+        }
+        l3->next = temp;
+        l3 = temp;
+      }
+      while(l1!=NULL) {
+        ListNode *temp = new ListNode;
+        temp->val = l1->val;
+        l1 = l1->next;
+        l3->next = temp;
+        l3 = temp;
+      }
+      while(l2!=NULL) {
+        ListNode *temp = new ListNode;
+        temp->val = l2->val;
+        l2 = l2->next;
+        l3->next = temp;
+        l3 = temp;
+      }      
+      return head->next;
+    }
+};
