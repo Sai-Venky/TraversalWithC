fa3c41adfe1ae8240ac54603d4510b1aa9a40c9a
Sai Venkatesh <venkysai.96@gmail.com>
2021-03-30 00:09:25 +0530

    Adding merge-strings-alternately solution

---
+++ challenges/merge-strings-alternately.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/merge-strings-alternately
+
+class Solution {
+public:
+    string mergeAlternately(string a, string b) {
+        int i = 0, j = 0, M = a.size(), N = b.size();
+        string ans;
+        while (i < M || j < N) {
+            if (i < M) ans += a[i++];
+            if (j < N) ans += b[j++];
+        }
+        return ans;
+    }
+};
