e20db69621d6259ecef370b88cf6bae1b3b2f449
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-02 13:27:40 +0530

    Adding intersection-of-two-arrays-ii solution

---
+++ challenges/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> count;
+        vector<int> inter;
+      
+      for(auto num:nums1) { count[num]++;}
+      for(auto num:nums2) { 
+        if(count[num]) {
+          inter.push_back(num);
+          count[num]--;
+        }
+      }
+      return inter;
+    }
+};
