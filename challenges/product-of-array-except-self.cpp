2a7c2de06f5db15110245ec217d481d851e06e7f
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-11 21:03:01 +0530

    Adding product-of-array-except-self solution

---
+++ challenges/product-of-array-except-self.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/product-of-array-except-self
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+      vector<int> output(nums.size(), 1);
+      int start=1, end=1;
+      
+      for(int i=0;i<nums.size();i++) {
+        output[i]*=start;
+        start*=nums[i];
+      }
+      
+      for(int i=nums.size()-1;i>=0;i--) {
+        output[i]*=end;
+        end*=nums[i];
+      }
+      
+      return output;
+    }
+};
