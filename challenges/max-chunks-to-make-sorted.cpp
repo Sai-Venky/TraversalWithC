6af5d94c7026ee1f348ba16aaa220eafab42145b
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-12 08:19:42 +0530

    Adding max-chunks-to-make-sorted solution

---
+++ challenges/max-chunks-to-make-sorted.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/max-chunks-to-make-sorted/
+
+
+class Solution {
+public:
+  int maxChunksToSorted(vector<int>& arr) {
+    int sum1 = 0, sum2 = 0, ans = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        sum1 += i;
+        sum2 += arr[i];
+        if(sum1 == sum2)    ans++;
+    }
+	  return ans;
+    }
+};
