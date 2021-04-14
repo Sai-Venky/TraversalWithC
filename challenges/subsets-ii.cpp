b5dbf82e578badcc07117c96a8d68b39e9e62c40
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-14 14:47:55 +0530

    Adding subsets-ii solution

---
+++ challenges/subsets-ii.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/subsets-ii/
+
+class Solution {
+public:
+    vector<vector<int>> outputs;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+      vector<int> output;
+      sort(nums.begin(), nums.end());
+      findSubsets(nums, 0, output);
+      return outputs;
+    }
+  
+    void findSubsets(vector<int>& nums, int start, vector<int>& output) {
+      outputs.push_back(output);
+
+      for(int i=start;i<nums.size();i++) {
+        if(i > start && nums[i] == nums[i-1]) continue; 
+        output.push_back(nums[i]);
+        findSubsets(nums, i+1, output);
+        output.pop_back();
+      }
+    }
+};
