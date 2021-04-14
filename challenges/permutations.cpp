e64a4d599955681f19817ecc89219a947ea037a5
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-14 15:06:27 +0530

    Adding permutations solution

---
+++ challenges/permutations.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/permutations
+
+class Solution {
+public:
+    vector<vector<int>> outputs;
+    vector<vector<int>> permute(vector<int>& nums) {
+      vector<int> output;
+      sort(nums.begin(), nums.end());
+      findPermutations(nums, output);
+      return outputs;        
+    }
+  
+    void findPermutations(vector<int>& nums, vector<int>& output) {
+      if(output.size()==nums.size()) {
+        outputs.push_back(output);
+        return;
+      }
+
+      for(int i=0;i<nums.size();i++) {
+        if(count(output.begin(), output.end(), nums[i])) continue; 
+        output.push_back(nums[i]);
+        findPermutations(nums, output);
+        output.pop_back();
+      }
+    }
+};
