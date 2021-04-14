82f60df377a6338b6d683873a31782e8e633a39b
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-14 15:31:23 +0530

    Adding permutations-ii solution

---
+++ challenges/permutations-ii.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/permutations-ii
+
+class Solution {
+public:
+    vector<vector<int>> outputs;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+      vector<int> output;
+      sort(nums.begin(), nums.end());
+      findPermutations(nums, output);
+      return outputs;        
+    }
+  
+    void findPermutations(vector<int>& nums, vector<int>& output) {
+      if(output.size()==nums.size()) {
+        outputs.push_back(output);
+        return;
+      }
+
+      for(int i=0;i<nums.size();i++) {
+        if(i>0 && nums[i] == nums[i-1]) continue; 
+        if(count(output.begin(), output.end(), nums[i]) == count(nums.begin(), nums.end(), nums[i])) continue; 
+        output.push_back(nums[i]);
+        findPermutations(nums, output);
+        output.pop_back();
+      }
+    }
+};
