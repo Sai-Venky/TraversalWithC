12e9eefd62a7fe13cdfe7129f9f4fbd42c5f2ed3
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-14 22:52:29 +0530

    Adding swap-nodes-in-pairs solution

---
+++ challenges/swap-nodes-in-pairs.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/swap-nodes-in-pairs
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swap(ListNode* first, ListNode* second) {  
+      if(second && first) { 
+        ListNode* third = second->next;
+        ListNode* fourth = NULL;
+        second->next=first;
+        fourth = third?third->next:fourth;
+        first->next = swap(third, fourth);
+      } else {
+        return first;
+      }
+
+      return second;
+    }
+  
+    ListNode* swapPairs(ListNode* head) {
+      if(!head || !head->next) return head;
+      ListNode* first=head;
+      ListNode* second=head->next;
+      
+      return swap(first, second);
+    }
+};
