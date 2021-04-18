933b1fcbb85da0c0b7e2f9885bcc273010e03751
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-18 14:45:45 +0530

    Adding house-robber solution

---
+++ challenges/house-robber.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/house-robber
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+      int i;
+      if(nums.size() == 1) {
+        return nums[0];
+      } else if (nums.size() == 2) {
+        return max(nums[0], nums[1]);
+      } else if (nums.size() == 3) {
+        return max(nums[0] + nums[2], nums[1]);
+      }
+      vector<int> dp(nums.size(), 0);
+      dp[0] = nums[0];
+      dp[1] = nums[1];
+      dp[2] = nums[0] + nums[2];
+      for(i=3;i<nums.size();i++) {
+        dp[i] = max({nums[i] + dp[i-2], dp[i-1], dp[i-3] + nums[i]});
+      }
+      
+      return dp[i-1];
+    }
+};
