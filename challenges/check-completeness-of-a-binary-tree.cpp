bb65f5d356a578abf7be2ed1c9cf2364552e567f
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-19 00:55:45 +0530

    Adding check-completeness-of-a-binary-tree solution

---
+++ challenges/check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/check-completeness-of-a-binary-tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+      queue<TreeNode*> q;
+      bool value = false;
+      q.push(root);
+      while(!q.empty()) {
+        TreeNode *p = q.front();
+        q.pop();
+        if(p==NULL) {
+                value=true;
+        } else {
+            if(value)
+                return false;
+            q.push(p->left);
+            q.push(p->right);
+        }
+      }
+      return true;
+    }
+};
