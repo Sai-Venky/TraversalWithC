3914c1bfb7a81d47292d493c01abb9ff9c99abc5
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-26 10:15:21 +0530

    Adding flatten-a-multilevel-doubly-linked-list solution

--- challenges/flatten-a-multilevel-doubly-linked-list.cpp
+++ challenges/flatten-a-multilevel-doubly-linked-list.cpp
@@ -36,3 +36,25 @@ public:
       flattenDLL(r);
     }
 };
+
+/*
+
+Node* flatten(Node* head) {
+	for (Node* h = head; h; h = h->next)
+	{
+		if (h->child)
+		{
+			Node* next = h->next;
+			h->next = h->child;
+			h->next->prev = h;
+			h->child = NULL;
+			Node* p = h->next;
+			while (p->next) p = p->next;
+			p->next = next;
+			if (next) next->prev = p;
+		}
+	}
+	return head;
+}
+
+/*
