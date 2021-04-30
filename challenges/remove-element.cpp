4f547ff43ec49899afe0583607b83507a11f24db
Sai Venkatesh <venkysai.96@gmail.com>
2021-04-30 20:38:03 +0530

    Adding remove-element solution

---
+++ challenges/remove-element.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/remove-element/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+      int begin=0;
+      for(int i=0;i<nums.size();i++) if(nums[i]!=val) nums[begin++]=nums[i];
+      return begin;
+    }
+};
