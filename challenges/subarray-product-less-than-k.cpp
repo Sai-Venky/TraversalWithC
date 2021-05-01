a539332af02b74db1c3a211050dfc9e478673c23
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-01 08:52:03 +0530

    Adding subarray-product-less-than-k solution

---
+++ challenges/subarray-product-less-than-k.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/subarray-product-less-than-k/
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+      int m=0, j=0;
+      long int prod=1;
+      for(int i=0;i<nums.size();i++) {
+        prod=prod*nums[i];
+        while(j<=i && prod >= k) {prod=prod/nums[j++];}
+        m+=i-j+1;
+        
+      }
+      
+      return m;
+    }
+};
