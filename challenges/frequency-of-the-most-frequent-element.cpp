540067b680f820f4560179111c0aac7c534a9e43
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-01 08:14:41 +0530

    Adding frequency-of-the-most-frequent-element solution

---
+++ challenges/frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/frequency-of-the-most-frequent-element
+
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+      sort(nums.begin(), nums.end());
+      long int m=1;
+      long int sum=0, j=0;
+      for(int i=0;i<nums.size();i++) {
+        sum+=nums[i];
+        while(((i-j+1)*nums[i]) - sum > k) {sum-=nums[j++];}
+        m = max(m, i-j+1);
+        
+      }
+      
+      return m;
+    }
+};
