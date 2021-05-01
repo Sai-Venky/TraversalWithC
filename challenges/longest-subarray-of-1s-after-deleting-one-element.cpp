9c0eede1518f280d6573f2c5e963216f3d9a3eec
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-01 08:33:44 +0530

    Adding longest-subarray-of-1s-after-deleting-one-element solution

---
+++ challenges/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+      
+      long int m=0, sum=0, j=0;
+      for(int i=0;i<nums.size();i++) {
+        sum+=nums[i];
+        if((i-j+1) - sum > 1) {sum-=nums[j++];}
+        m = max(m, i-j);
+      }
+      
+      return m;
+    }
+};
+
