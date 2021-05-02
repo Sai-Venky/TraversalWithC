71ec31616bb0e6c022660ffc792896ac250b4c5e
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-02 15:49:57 +0530

    Adding number-of-substrings-containing-all-three-characters solution

---
+++ challenges/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+      vector<int> v{0,0,0};
+      int ct=0, j=0;
+      for(int i=0; i<s.length();i++) {
+        v[s[i] - 'a']++;
+        while(v[0] && v[1] && v[2]) { v[s[j] - 'a']--;j++;}
+        ct+=j;
+      }
+      return ct;
+    }
+};
