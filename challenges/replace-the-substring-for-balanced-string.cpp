4a83c37f3d9e7bdc635dc8ddffa24e1854aad46e
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-02 22:05:35 +0530

    Adding replace-the-substring-for-balanced-string solution

---
+++ challenges/replace-the-substring-for-balanced-string.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/replace-the-substring-for-balanced-string
+
+class Solution {
+public:
+    int balancedString(string s) {
+      map<char, int> mp;
+      for(auto c:s) mp[c]++;
+      int n = s.length()/4;
+      int j=0;
+      int window = s.length();
+      for(int i=0;i<s.length();i++){
+        mp[s[i]]--;
+        while(mp['Q'] <= n && mp['W'] <= n && mp['E'] <= n && mp['R'] <= n && s.length()>j) {
+          mp[s[j]]++;
+          window=min(window, i-j+1);
+          j++;
+        }
+      }
+      return window;
+    }
+};
