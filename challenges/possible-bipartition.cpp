6961b1f0e9f416acb37a9280645be377ed1ba68d
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-08 22:25:32 +0530

    Adding possible-bipartition solution

---
+++ challenges/possible-bipartition.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/possible-bipartition/
+
+class Solution {
+public:
+    bool possibleBipartition(int N, vector<vector<int>>& dislikes) {
+      
+      vector<int> color(N, 0);
+      
+      vector<vector<int>> v(N, vector<int>());
+      
+      for(auto d:dislikes) {
+        v[d[0]-1].push_back(d[1]-1); 
+        v[d[1]-1].push_back(d[0]-1); 
+      }
+      
+      
+      for(int i=0; i<N; i++) {
+        if(color[i] != 0){
+          continue;
+        }
+        queue<int> q;
+        
+        q.push(i);
+        color[i] = 1;
+        while(!q.empty()) {
+          int l = q.front();
+          vector<int> dislike = v[l];
+          q.pop();
+          for(auto val: dislike) {
+            
+            if(color[val] == 0) {
+              color[val] = -1 *color[l];
+              q.push(val);
+            } else if(color[val] == color[l]) {
+              return false;
+            }
+          }
+
+        }
+        
+      }
+      
+      return true;
+    }
+};
