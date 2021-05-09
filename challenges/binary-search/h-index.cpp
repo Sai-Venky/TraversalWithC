f8b06be0410ea2bef38da32a5c2e302306fc5bfe
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-09 23:20:45 +0530

    Adding h-index solution

---
+++ challenges/binary-search/h-index.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/h-index/
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+      sort(citations.begin(), citations.end());
+      int left=0, len = citations.size(), right= len-1,  mid;
+      while(left<=right)
+      {
+          mid=left+ (right-left)/2;
+          if(citations[mid] >= (len-mid)) right = mid - 1;
+          else left = mid + 1;
+      }
+      return len - left;
+  }
+};
