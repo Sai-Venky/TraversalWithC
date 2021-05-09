394627458119e10705b1d2aa3f6d2a54ecbf68b4
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-09 21:56:56 +0530

    Adding find-smallest-letter-greater-than-target solution

---
+++ challenges/binary-search/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+      int low=0, high=letters.size()-1;
+      char res = letters[0];
+      while(low<=high) {
+        
+        int mid = low + (high-low)/2;
+        if(letters[mid] > target) {
+          res = letters[mid];
+          high=mid-1;
+        } else {
+          low=mid+1;
+        }
+        
+      }
+      return res;
+    }
+};
