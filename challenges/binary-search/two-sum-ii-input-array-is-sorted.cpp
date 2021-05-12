3922f27d3203f4b25459e9c693dbdd2cb75f13f5
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-12 09:51:30 +0530

    Adding two-sum-ii-input-array-is-sorted solution

---
+++ challenges/binary-search/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+      int l=0, h=numbers.size()-1;
+      vector<int> out;
+      while(l<=h) {
+        if(l!=h && numbers[l] + numbers[h] == target) {
+          out = vector<int>{l+1, h+1};
+          break;
+        } else if(numbers[l] + numbers[h] > target) {
+          h--;
+        } else {
+          l++;
+        }
+      }
+      return out;
+    }
+};
