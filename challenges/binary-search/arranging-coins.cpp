1ce950159e3acf1a56c30325c75da2772c9bf0ec
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-12 10:36:36 +0530

    Adding arranging-coins solution

---
+++ challenges/binary-search/arranging-coins.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/arranging-coins/
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+      
+      long l=0, h =n, m;
+      while(l<=h) {
+        m = l +(h-l)/2;
+        if(m*(m+1) > 2 * long(n)) {
+          h=m-1;
+        } else {
+          l=m+1;
+        }
+      }
+      return h;
+      
+    }
+};
