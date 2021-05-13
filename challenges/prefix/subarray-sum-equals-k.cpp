218fe3e23eaf30cf9257e80e0c9c66aece8a1dc1
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-13 10:12:34 +0530

    Adding subarray-sum-equals-k solution

---
+++ challenges/prefix/subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/subarray-sum-equals-k
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+      map<int, int> prefix;
+      int ct=0, s=0;
+      prefix[0]=1;
+      for(int i=0;i<nums.size();i++) {
+        s += nums[i];
+        ct+=prefix[s - k];
+        prefix[s]+=1;
+
+      }
+      
+      return ct;
+    }
+};
