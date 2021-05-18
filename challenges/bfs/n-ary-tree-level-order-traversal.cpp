98180ea40cd179e27ffc91b146fac0a30d07d6b6
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-18 22:04:55 +0530

    Adding n-ary-tree-level-order-traversal solution

---
+++ challenges/bfs/n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/n-ary-tree-level-order-traversal/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+      queue<Node*> q;
+      vector<vector<int>> outputs;
+      if(root==NULL) return outputs;
+      q.push(root);
+      
+      while(!q.empty()) {
+        vector<int> output;
+        int size=q.size();
+        for(int i=0;i<size;i++) {
+          Node* node = q.front();
+          
+          for(auto n:node->children) {
+            q.push(n);
+          }
+          output.push_back(node->val);
+          q.pop();
+          
+        }
+        outputs.push_back(output);
+      }
+      return outputs;
+    }
+};
