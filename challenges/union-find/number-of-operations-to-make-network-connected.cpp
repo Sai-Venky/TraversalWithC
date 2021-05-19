5a69379cadbc6a91e8c58daaf9720ee6a4e7c969
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-19 13:43:07 +0530

    Moving solution

--- challenges/union-find/number-of-operations-to-make-network-connected.cpp
+++ challenges/union-find/number-of-operations-to-make-network-connected.cpp
@@ -4,6 +4,53 @@
 /*
     Important to note that To connect all nodes need at least n-1 edges
 */
+
+/*
+
+    BFS 
+    class Solution {
+public:
+    void bfs(int i,vector<int>&visited,vector<vector<int>>&mat)
+    {
+        visited[i]=1;
+        queue<int>q;
+        q.push(i);
+        while(!q.empty())
+        {
+            int k = q.front();
+            q.pop();
+            for(auto c: mat[k])
+            {
+                if(!visited[c])
+                {
+                    visited[c]=1;
+                    q.push(c);
+                }
+            }
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size()<n-1) return -1; //if number of connections are less than required number of edges to make the graph connected, then simply return -1 as it is not possible.
+        vector<vector<int>>mat(n); 
+        for(auto c: connections)
+        {
+        mat[c[0]].push_back(c[1]);
+        mat[c[1]].push_back(c[0]);
+        }
+        vector<int>visited(n,0);
+        int count = 0;
+        for(int i=0;i<n;i++)
+        {
+          if(visited[i]==0)
+          {
+              count++;
+              bfs(i,visited,mat);
+          }
+        }
+        return count-1; //if there are n components then n-1 wires are needed to make it connected.
+    }
+};
+*/
 class Solution {
 public:
   vector<int>parent;
