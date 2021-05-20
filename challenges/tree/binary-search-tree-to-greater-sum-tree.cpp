40b7f0283a40db85dfb6bd67d670001204e28c0d
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-20 21:35:21 +0530

    Adding convert-bst-to-greater-tree solution

---
+++ challenges/tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void replaceTree(TreeNode* root, int *sum){
+      if(!root) return;
+      
+      replaceTree(root->right, sum);
+      root->val = *sum + root->val;
+      *sum = root->val;
+      replaceTree(root->left, sum);
+    }
+  
+    TreeNode* bstToGst(TreeNode* root) {
+      int sum=0;
+      replaceTree(root, &sum);
+      return root;
+    }
+};
---
+++ challenges/tree/convert-bst-to-greater-tree.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/convert-bst-to-greater-tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void replaceTree(TreeNode* root, int *sum){
+      if(!root) return;
+      
+      replaceTree(root->right, sum);
+      root->val = *sum + root->val;
+      *sum = root->val;
+      replaceTree(root->left, sum);
+    }
+  
+    TreeNode* convertBST(TreeNode* root) {
+      int sum=0;
+      replaceTree(root, &sum);
+      return root;
+    }
+};
