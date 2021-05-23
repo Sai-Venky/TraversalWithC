3da42f13f525550c5161895087cf1e67f8af9526
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-23 23:50:51 +0530

    Adding russian-doll-envelopes solution

--- challenges/dp/russian-doll-envelopes.cpp
+++ challenges/dp/russian-doll-envelopes.cpp
@@ -1,5 +1,29 @@
 // https://leetcode.com/problems/russian-doll-envelopes
 
+
+/*
+class Solution {
+public:
+    static bool compare(vector<int>&a , vector<int>& b)
+    {
+      return a[0]==b[0]?a[1]>b[1]:a[0]<b[0];
+    }
+    int maxEnvelopes(vector<vector<int>>&a) {
+      sort(a.begin(),a.end(),compare);
+      vector<int>dp;
+      for(auto i:a)
+      {
+        auto it=lower_bound(dp.begin(),dp.end(),i[1]);
+        if(it==dp.end())
+          dp.push_back(i[1]);
+        else
+          *it=i[1];
+        cout<<dp.size()<<" ";
+      }
+      return dp.size();
+    }
+};
+*/
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& es) {
