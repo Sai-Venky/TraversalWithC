fbc7a3ec0532d77bb9df999d5357537a779850b1
Sai Venkatesh <venkysai.96@gmail.com>
2021-05-27 08:59:02 +0530

    Adding dungeon-game solution

---
+++ challenges/dp/dungeon-game.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/dungeon-game
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+      int m=dungeon.size(), n=dungeon[0].size();
+      vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MAX));
+      dp[m-1][n]=1;
+      dp[m][n-1]=1;
+      int ct=0,i,j;
+      for(i=m-1;i>=0;i--) {
+        for(j=n-1;j>=0;j--) {
+          
+          dp[i][j] = min(dp[i][j+1], dp[i+1][j]) - dungeon[i][j];
+          dp[i][j] = dp[i][j]<=0?1:dp[i][j];
+        }
+      }
+      
+      return dp[0][0];
+      
+    }
+};
