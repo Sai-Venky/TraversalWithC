6e80913f3dd86d8569ac11c69fb99f2cfe7a1a52
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-05 23:56:11 +0530

    Adding maximum-subarray-sum-with-one-deletion solution

--- challenges/dp/kandane-alg/maximum-subarray-sum-with-one-deletion.cpp
+++ challenges/dp/kandane-alg/maximum-subarray-sum-with-one-deletion.cpp
@@ -7,12 +7,10 @@ public:
     vector<int> keep(n), del(n);
 
     keep[0] = del[0] = arr[0];
-    cout<<keep[0]<<" "<<del[0]<<"\n";
     
     for (int i = 1; i < n; i++) {
       keep[i] = max(keep[i - 1], 0) + arr[i]; 
       del[i] = max(keep[i - 1], del[i - 1] + arr[i]);
-      cout<<keep[i]<<" "<<del[i]<<"\n";
       res = max({ res, keep[i], del[i] });
     }
     return res;
