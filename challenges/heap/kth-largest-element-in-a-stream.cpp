b0eca199bc78b34972d35c3f856c6f66b630ecd2
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-09 10:27:18 +0530

    Adding kth-largest-element-in-a-stream solution

---
+++ challenges/heap/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/kth-largest-element-in-a-stream
+
+class KthLargest {
+  int k_;
+  priority_queue<int, vector<int>, greater<int>> q;
+public:
+    KthLargest(int k, vector<int>& nums) {
+      k_=k;
+      for(int num:nums) {
+        q.push(num);
+      }
+    }
+    
+    int add(int val) {
+      q.push(val);
+      while(q.size()>k_) {
+        q.pop();
+      }
+      return q.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
