922a879dc42d87baf3ec5267abfd7833b342bf53
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-09 23:18:12 +0530

    Adding k-closest-points-to-origin solution

---
+++ challenges/heap/k-closest-points-to-origin.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/k-closest-points-to-origin
+
+class Solution {
+public:
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+      priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+      vector<vector<int>> output;
+      for(int i=0;i<points.size();i++) {
+        int dist = (points[i][1]*points[i][1])+(points[i][0]*points[i][0]);
+        q.push(make_pair(dist, i));
+      }
+      
+      while(k>0) {
+        pair<int, int> p = q.top();
+        q.pop();
+        output.push_back(points[p.second]);
+        k--;
+      }
+      
+      return output;
+    }
+};
