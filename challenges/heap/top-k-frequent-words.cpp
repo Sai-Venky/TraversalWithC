b416e3edd1917d671e91170a7cedd54c350cb33d
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-09 23:58:36 +0530

    Adding top-k-frequent-words solution

---
+++ challenges/heap/top-k-frequent-words.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/top-k-frequent-words/
+
+class Solution {
+public:
+    struct Frequency {
+      bool operator()(const pair<string, int> &p1, const pair<string, int> &p2) {
+        if(p1.second == p2.second) return p1.first>p2.first;
+        return p1.second < p2.second;
+      }
+    };
+    priority_queue<pair<string, int>, vector<pair<string, int>>, Frequency> q;
+  
+    vector<string> topKFrequent(vector<string>& words, int k) {
+      vector<string> out;
+      map<string, int> mp;
+      for(auto str:words) mp[str]++;
+      
+      for(auto val:mp)  {
+        q.push({val.first, val.second});
+      }
+      
+      while(!q.empty() && k>0) {
+        pair<string, int> p = q.top();
+        q.pop();
+        out.push_back(p.first);
+        k--;
+      }
+      return out;
+    }
+};
