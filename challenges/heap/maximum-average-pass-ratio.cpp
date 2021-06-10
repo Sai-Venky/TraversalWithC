f7831888f2222473688f9e38047ac1a20c0f3790
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-10 16:59:05 +0530

    Adding maximum-average-pass-ratio solution

---
+++ challenges/heap/maximum-average-pass-ratio.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/maximum-average-pass-ratio/
+
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+      
+      priority_queue<pair<double, int>> q;
+      
+      for(int i=0;i<classes.size();i++) {
+        double gain = ((double(classes[i][0]+1))/(classes[i][1]+1))  - (double(classes[i][0])/classes[i][1]);
+        q.push(make_pair(gain, i));
+      }
+           
+      while(extraStudents>0) {
+        pair<double, int> p = q.top();q.pop();
+        int i = p.second;
+        classes[i][0] = classes[i][0]+1;
+        classes[i][1] = classes[i][1]+1;
+        double gain = ((double(classes[i][0])+1)/(classes[i][1]+1))  - (double(classes[i][0])/classes[i][1]);
+        q.push(make_pair(gain, i));
+        extraStudents--;
+      }
+      
+      double av=0;
+      for(auto cl:classes) {
+        av+= (double(cl[0])/cl[1]);
+      }
+      
+      return av/classes.size();
+                      
+    }
+};
