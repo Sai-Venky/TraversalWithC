1511334a4cc96c3a6e63567e423aab7396d54ae7
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-11 18:30:56 +0530

    Adding furthest-building-you-can-reach solution

---
+++ challenges/heap/furthest-building-you-can-reach.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/furthest-building-you-can-reach
+
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+      
+      priority_queue<int> q;
+            
+      int i=1;
+      for(i=1;i<heights.size();i++) {
+        int need = heights[i]-heights[i-1];
+        if(need>0) {
+          q.push(need);
+          if(bricks < need) {
+            if(ladders==0) {
+              return i-1;
+            }
+            bricks+=q.top();
+            q.pop();
+            ladders--;
+          }
+          bricks-=need;
+        }
+        
+      }
+      
+      return i-1;
+      
+    }
+};
