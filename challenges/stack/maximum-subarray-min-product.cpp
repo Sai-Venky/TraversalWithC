1fb1fc75496bcb74e4fe063f2c2fbca4128bf5bb
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-14 08:13:10 +0530

    Adding maximum-subarray-min-product solution

--- challenges/stack/maximum-subarray-min-product.cpp
+++ challenges/stack/maximum-subarray-min-product.cpp
@@ -5,8 +5,10 @@ public:
     int maxSumMinProduct(vector<int>& n) {
       long res = 0;
       vector<long> dp(n.size() + 1), st;
+      
       for (int i = 0; i < n.size(); ++i)
          dp[i + 1] = dp[i] + n[i];
+
       for (int i = 0; i <= n.size(); ++i) {
           while (!st.empty() && (i == n.size() || n[st.back()] > n[i])) {
               int j = st.back();
