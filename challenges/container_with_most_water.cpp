06af5941f5ff74c6aee17f5926b6029ceee18c9c
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-15 20:41:44 +0530

    Adding container-with-most-water solution

--- challenges/container_with_most_water.cpp
+++ challenges/container_with_most_water.cpp
@@ -1,43 +1,33 @@
 // https://leetcode.com/problems/container-with-most-water/
 
+/*
+Consider a simpler problem. Let's say there's a table with a bunch of closed boxes. You're told there's a number in each box and you're asked to find the largest number. So you open some first box, remember its number, and then throw that box away. Then you open some other box. If its number is larger than the one before, you remember this new number instead. And then you throw that box away. And so on. Until you went through all boxes. In code, that might look like this:
+
+max = 0
+i = 0
+while i < number_of_boxes:
+    if number_in_box[i] > max:
+        max = number_in_box[i]
+    i = i + 1
+print(max)
+Are you here also wondering whether the optimal solution might be missed? If yes, then why? If no, then what's the difference to the water problem? The box problem is simpler, but the process is the same as the water one. You consider a new option, you update your current "best", you remove something that can't give you something better anymore, and you move on. If you have seen the overall optimum already, then you have seen it already and remember it until the end. And if not, it's still in the game. So it will be found at some point. Really the only thing you need to show is that you don't make a mistake when you reduce the problem, which is what I did in my post.
+ it does not matter whether you perform i++ or j--, because if h[i] == h[j], neither (i+1, j) or (i, j-1) can be potential solutions because the area obtained is necessarily smaller than (i, j).
+*/
+
 class Solution {
 public:
-    
-    int min(int a, int b) {
-        if(a>b) {
-            return b;
-        } else {
-            return a;
-        }    
-    }
-    int area (vector<int>& height, int i, int j) {
-        if(i>=j) {
-            return 0;
-        }
-        return (j-i) * min(height[i], height[j]);
-    }
-    
-    void movePointer(vector<int>& height, int *i, int *j) {
-        int area1 = height[*i];
-        int area2 = height[*j];
-        if(area1>=area2) {
-            *j = *j-1;
-        } else if(area2>area1) {
-            *i = *i+1;
-        }
-    }
     int maxArea(vector<int>& height) {
-        int length = height.size();
-        int i=0, j=length-1;
-        int max_area=0;
-        while(i<j) {
-            int area_ = area(height, i, j);
-            if(area_>max_area) {
-                max_area = area_;
-            }
-            movePointer(height, &i ,&j);
-        }
+      
+      int l=0, h=height.size()-1;
+      int marea=0;
+      
+      while(l<h) {
+        marea = max(marea, (h-l) * min(height[l], height[h]));
         
-        return max_area; 
+        if(height[l] > height[h]) h--;
+        else l++;
+      }
+      
+      return marea;
     }
 };
