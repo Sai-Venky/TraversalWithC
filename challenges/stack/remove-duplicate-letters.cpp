2ce0b5eb93b15e8a11990512e1fadc579857f5a6
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-17 12:22:25 +0530

    Adding smallest-subsequence-of-distinct-characters solution

---
+++ challenges/stack/remove-duplicate-letters.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/remove-duplicate-letters/
+
+class Solution {
+public:
+    string removeDuplicateLetters(string str) {
+      
+      map<char, int> m;
+      map<char, bool> vis;
+      stack<char> s;
+      string out;
+      for(auto c:str) m[c]++;
+
+      
+      for(int i=0;i<str.size();i++) {
+      
+        m[str[i]]--;
+        if(vis[str[i]]) continue;
+        
+        while(!s.empty() && m[s.top()]>0 && s.top() > str[i]) {
+          out.pop_back();
+          vis[s.top()] = false;
+          s.pop();
+        }
+        s.push(str[i]);
+        out.push_back(str[i]);
+        vis[str[i]] = true;
+      }
+      
+      return out;
+    }
+};
---
+++ challenges/stack/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/smallest-subsequence-of-distinct-characters
+
+class Solution {
+public:
+    string smallestSubsequence(string str) {
+      
+      map<char, int> m;
+      map<char, bool> vis;
+      string out;
+      for(auto c:str) m[c]++;
+
+      
+      for(int i=0;i<str.size();i++) {
+      
+        m[str[i]]--;
+        if(vis[str[i]]) continue;
+        
+        while(!out.empty() && m[out.back()]>0 && out.back() > str[i]) {
+          vis[out.back()] = false;
+          out.pop_back();
+        }
+        out.push_back(str[i]);
+        vis[str[i]] = true;
+      }
+      
+      return out;
+    }
+};
