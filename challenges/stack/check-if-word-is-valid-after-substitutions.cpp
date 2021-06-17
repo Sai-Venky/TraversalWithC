593f9229a81a4b96d3b8836ba9578301ff5d3582
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-18 00:17:20 +0530

    Adding check-if-word-is-valid-after-substitutions solution

---
+++ challenges/stack/check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/check-if-word-is-valid-after-substitutions
+
+class Solution {
+public:
+    bool isValid(string s) {
+      
+      stack<int> t;
+      
+      for(int i=0;i<s.length();i++) {
+        
+        if(s[i] == 'a') {
+          t.push(0);
+        } else if(s[i] == 'b') {
+          if(t.empty()) return false;
+          t.top()++;
+        } else {
+          if(t.empty() || t.top()!=1) return false;
+          t.pop();
+        }
+      }
+      
+      return t.empty()?true:false;
+    }
+};
