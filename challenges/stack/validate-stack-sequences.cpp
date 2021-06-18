31ef52c3d6213deed8fb5294e2df36a532a5c2c2
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-18 21:47:23 +0530

    Adding validate-stack-sequences solution

--- challenges/stack/validate-stack-sequences.cpp
+++ challenges/stack/validate-stack-sequences.cpp
@@ -1,5 +1,20 @@
 // https://leetcode.com/problems/validate-stack-sequences
 
+
+/*
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> stack;
+        int i = 0;
+        for (int x : pushed) {
+            stack.push(x);
+            while (stack.size() && stack.top() == popped[i]) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.size() == 0;
+    }
+*/
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
