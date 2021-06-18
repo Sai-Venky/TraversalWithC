65d0d264f52e829661d4ec07e41f56c71e350118
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-18 23:30:22 +0530

    Adding maximum-frequency-stack solution

---
+++ challenges/stack/maximum-frequency-stack.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/maximum-frequency-stack/
+
+class FreqStack {
+public:
+    unordered_map<int,int> freq;
+    unordered_map<int,stack<int>> m;
+    int maxf=0;
+    
+    void push(int val) {
+      maxf=max(maxf, ++freq[val]);
+      m[freq[val]].push(val);
+    }
+    
+    int pop() {
+      int val=m[maxf].top();
+      m[maxf].pop();
+      if (!m[freq[val]--].size()) maxf--;
+      return val;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
