08cb91cf37639197b5f63dda08194b7ed34cea07
Sai Venkatesh <venkysai.96@gmail.com>
2021-06-23 21:24:05 +0530

    Adding construct-binary-search-tree-from-preorder-traversal solution

---
+++ challenges/tree/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructTree(vector<int>& preorder, int s, int e) {
+      if(s>e) return NULL;
+      int ele = preorder[s], i=s;
+      TreeNode* root = new TreeNode(ele);
+      for(; i<=e; i++) {
+        if(preorder[i]>ele) break;
+      }
+      root->left  = constructTree(preorder, s+1, i-1);
+      root->right = constructTree(preorder, i, e);
+      
+      return root;
+    }
+  
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+      
+      return constructTree(preorder, 0, preorder.size()-1);
+    }
+};
