1e5b09fc6513cd86cfb6c97b633ad88718a7b67a
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-01 23:23:29 +0530

    Adding maximum-ice-cream-bars solution

---
+++ challenges/array/maximum-ice-cream-bars.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-ice-cream-bars
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+      
+      sort(costs.begin(), costs.end());
+      int maximum=0;
+      
+      for(int i=0;i<costs.size();i++) {
+        
+        coins-=costs[i];
+        if(coins<0) break;
+        maximum++;
+      }
+      
+      return maximum;
+    }
+};
