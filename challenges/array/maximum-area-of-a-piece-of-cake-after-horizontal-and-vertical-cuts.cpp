0e86e87bb222ee01b7c23d86b16f56c09fddca1f
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-02 15:17:22 +0530

    Adding maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts solution

---
+++ challenges/array/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/
+
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& hc, vector<int>& vc) {
+      
+
+      hc.insert(hc.begin(), 0);      
+      hc.push_back(h);
+      vc.insert(vc.begin(), 0);
+      vc.push_back(w);
+      sort(hc.begin(), hc.end());
+      sort(vc.begin(), vc.end());
+      int maxh=0, maxv=0;
+      for(int i=1;i<hc.size();i++) {
+        maxh=max(maxh, hc[i]-hc[i-1]);
+      }
+      
+      for(int i=1;i<vc.size();i++) {
+        maxv=max(maxv, vc[i]-vc[i-1]);
+      }
+      return (long(maxh)*maxv)%1000000007;
+    }
+};
