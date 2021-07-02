b56084475948b44099835542932f976de49bba16
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-02 12:19:33 +0530

    Adding set-matrix-zeroes solution

---
+++ challenges/array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/
+
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+      
+      int right=0, left=0, n=nums.size();
+      for(int num:nums) right+=num;
+      
+      for(int i=0;i<n;i++) {
+        right -= nums[i];
+        left += nums[i];
+        nums[i] = (((i+1)*nums[i])-left) + (right-((n-i-1)*nums[i]));
+      }
+      
+      return nums;
+    }
+};
