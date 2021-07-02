b7ea8dd82f802616e6c7561578e1f385bb65c6a7
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-02 15:41:56 +0530

    Adding array-of-doubled-pairs solution

---
+++ challenges/hashing/array-of-doubled-pairs.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/array-of-doubled-pairs
+
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        
+      unordered_map<int, int> m;
+      sort(arr.begin(),arr.end());
+      for(int a:arr) {
+        m[a]++;
+      }
+      
+      for(int i=0;i<arr.size();i++) {
+        if(m.find(2*arr[i]) !=m.end() && m.find(arr[i]) !=m.end()) {
+          m[arr[i]]--;
+          m[2*arr[i]]--;
+          if(m[arr[i]]==0) m.erase(arr[i]);
+          if(m[2*arr[i]]==0) m.erase(2*arr[i]);
+        }
+      }
+      
+      return m.empty();
+    }
+};
