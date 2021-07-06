3a8177ecf6ad9f00262f2f8d45edf8a3fbb1eabb
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-07 00:09:46 +0530

    Adding finding-the-users-active-minutes solution

---
+++ challenges/hashing/finding-the-users-active-minutes.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/finding-the-users-active-minutes
+
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+      vector<int> uam(k, 0);
+      map<int, set<int>> m;
+      
+      for(auto log:logs) {
+        m[log[0]].insert(log[1]);
+      }
+      
+      for(auto v:m) {
+        uam[v.second.size()-1]++;
+      }
+      
+      return uam;
+    }
+};
