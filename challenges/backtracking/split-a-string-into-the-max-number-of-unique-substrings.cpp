7d97fde9ad7fb561e6031b4866e598c54d2d8885
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-14 15:04:17 +0530

    adding split-a-string-into-the-max-number-of-unique-substrings solution

---
+++ challenges/backtracking/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings
+
+class Solution {
+public:
+    unordered_set<string> s;
+    int out=0;
+    int maxUniqueSplit(string str) {
+      split(str, 0);
+      return out;
+    }
+  
+    void split(string str, int start) {
+      
+      if(start==str.length()) {
+        if(s.size()>out) out=s.size();
+        return;
+      }
+      string comb="";
+      for(int i=start;i<str.length();i++) {
+        comb.push_back(str[i]);
+        if(s.count(comb)>0) continue;
+        s.insert(comb);
+        split(str, i+1);
+        s.erase(comb);
+      }
+      
+    }
+};
