4861a781f1f03f97ebf957b82fb6533d1ac125a9
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-15 09:53:35 +0530

    adding number-of-matching-subsequences solution

---
+++ challenges/backtracking/increasing-subsequences.cpp
@@ -0,0 +1,90 @@
+// https://leetcode.com/problems/increasing-subsequences
+
+
+/*
+    Iterative
+    class Solution {
+        public:
+            vector<vector<int>> findSubsequences(vector<int>& a) {
+                set<vector<int>> seqs = {vector<int>(0)};
+                for (int i = 0; i < a.size(); i++) {
+                    vector<vector<int>> built(seqs.size());
+                    std::copy(seqs.begin(), seqs.end(), built.begin());
+                    for (auto seq : built) {
+                        if (seq.empty() || a[i] >= seq.back()) {
+                            seq.push_back(a[i]);
+                            seqs.insert(seq);
+                        }
+                    }
+                }
+                
+                vector<vector<int>> res;
+                for (auto seq : seqs)
+                    if (seq.size() > 1) res.push_back(seq);
+
+                return res;
+            }
+        };
+*/ 
+
+/*
+    Backtracking
+    class Solution {
+        public:
+            vector<vector<int>> findSubsequences(vector<int>& nums) {
+                vector<vector<int>> res;
+                vector<int> seq;
+                dfs(res, seq, nums, 0);
+                return res;
+            }
+            
+            void dfs(vector<vector<int>>& res, vector<int>& seq, vector<int>& nums, int pos) {
+                if(seq.size() > 1) res.push_back(seq);
+                unordered_set<int> hash;
+                for(int i = pos; i < nums.size(); ++i) {
+                    if((seq.empty() || nums[i] >= seq.back()) && hash.find(nums[i]) == hash.end()) {
+                        seq.push_back(nums[i]);
+                        dfs(res, seq, nums, i + 1);
+                        seq.pop_back();
+                        hash.insert(nums[i]);
+                    }
+                }
+            }
+        };
+*/
+
+// Unoptimized
+class Solution {
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+      unordered_map<int, vector<vector<int>>> m;
+      set<vector<int>> s;
+      int n=nums.size();
+      vector<vector<int>> out;
+      
+      for(int i=0;i<n;i++) {
+        for(int j=0;j<i;j++) {
+          if(nums[i]>=nums[j]) {
+            if(!s.count({{nums[j], nums[i]}})) {
+              m[i].push_back({nums[j], nums[i]});
+              s.insert(m[i].back());
+            }
+            for (int k=0; k<m[j].size(); k++) {
+              vector<int> temp = m[j][k];
+              temp.push_back(nums[i]);
+              if(s.count(temp)) continue;
+              m[i].push_back(temp);
+              s.insert(m[i].back());
+            }
+              
+          }
+        }
+      }
+      
+      for(auto v:m) {
+        out.insert(out.end(), v.second.begin(), v.second.end());
+      }
+      
+      return out;
+    }
+};
---
+++ challenges/hashing/number-of-matching-subsequences.cpp
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/number-of-matching-subsequences
+
+
+/*
+    int numMatchingSubseq(string S, vector<string>& words) {
+        int cnt=0;
+        unordered_map<char,vector<string>> m;
+        for (auto& word: words) m[word.back()].push_back(word);
+        for (int i=(int)S.size()-1; i>=0; --i){
+            char c=S[i];
+            auto bucket=m[c];
+            m[c].clear();
+            for (auto& word: bucket){
+                word.pop_back();
+                if (word.empty())
+                    ++cnt;
+                else
+                    m[word.back()].push_back(word);
+            }
+        }
+        return cnt;
+    }
+*/
+
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+      int ct=0;
+      unordered_map<char, vector<int>> m;
+      
+      
+      for(int i=0;i<s.length();i++) {
+        m[s[i]].push_back(i);
+      }
+      
+      for(int i=0;i<words.size();i++) {
+        int pos=-1;
+        bool poss=true;
+        for(int j=0;j<words[i].length();j++) {
+          char c=words[i][j];
+          auto it=upper_bound(m[c].begin(), m[c].end(), pos);
+          if (it == m[c].end()) {
+            poss=false;
+            break;
+          } else {
+            pos=*it;
+          }
+        }
+        if(poss) ct++;
+      }
+        
+      
+      return ct;
+    }
+};
