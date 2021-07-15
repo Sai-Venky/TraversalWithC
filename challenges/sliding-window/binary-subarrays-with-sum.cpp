c321dd24aa8866e287bfb284ef2c701115dd3dbc
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-15 14:58:45 +0530

    adding binary-subarrays-with-sum solution

--- challenges/sliding-window/binary-subarrays-with-sum.cpp
+++ challenges/sliding-window/binary-subarrays-with-sum.cpp
@@ -1,4 +1,18 @@
 // https://leetcode.com/problems/binary-subarrays-with-sum/
+
+/*
+   int numSubarraysWithSum(vector<int>& A, int S) {
+        unordered_map<int, int> c({{0, 1}});
+        int psum = 0, res = 0;
+        for (int i : A) {
+            psum += i;
+            res += c[psum - S];
+            c[psum]++;
+        }
+        return res;
+    }
+*/
+
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& A, int S) {
