75939cf691b06125ffe41a606a659bf8087cf0ba
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-17 15:55:51 +0530

    adding path-sum solution

---
+++ challenges/tree/path-sum.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/path-sum
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root)return false;                                         
+        sum=sum-root->val;                                             
+        if(sum==0&&!root->left&&!root->right)return true;              
+        return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);
+    }
+};
