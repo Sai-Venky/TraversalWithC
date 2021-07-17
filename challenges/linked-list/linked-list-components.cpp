c0aebfaeb69a616b826b86913ebfb47440797cd8
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-17 14:57:45 +0530

    adding linked-list-components solution

---
+++ challenges/linked-list/linked-list-components.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/linked-list-components/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& G) {
+        unordered_set<int> subset;
+        int connected_components = 0;
+        
+        for(auto n : G)
+            subset.insert(n);
+        
+        while(head) {
+            if (subset.count(head->val) && (head->next == NULL || !subset.count(head->next->val)))
+                connected_components++;
+            head = head->next;
+        }   
+        return connected_components;
+    }
+};
