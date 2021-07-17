9c1e9bfcc1ff997ab53732d1b68cbe2f898d3075
Sai Venkatesh <venkysai.96@gmail.com>
2021-07-17 15:49:37 +0530

    adding minimum-depth-of-binary-tree solution

---
+++ challenges/tree/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+      int hgt=0;
+      if(root==NULL) return 0;
+      queue<TreeNode*> q;
+      q.push(root);
+      while(!q.empty()) {
+        int n=q.size();
+        hgt++;
+        for(int i=0;i<n;i++) {
+          TreeNode* node=q.front();q.pop();
+          if(node->left==NULL && node->right==NULL) return hgt;
+          if(node->left) q.push(node->left);
+          if(node->right) q.push(node->right);
+        }
+      }
+      return hgt;
+    }
+};
