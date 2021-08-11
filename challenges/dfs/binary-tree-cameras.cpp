f437e05a6a4c7033890d45ca02cf11300a159235
Sai Venkatesh <venkysai.96@gmail.com>
2021-08-12 00:34:17 +0530

    Adding binary-tree-cameras Solution

---
+++ challenges/dfs/binary-tree-cameras.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/binary-tree-cameras
+
+class Solution {
+public:
+    int minCameraCover(TreeNode* root) {
+        if (dfs(root) == 1) res++;
+        return res;
+    }
+    
+private:
+    int res = 0;
+    
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+        
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        
+        if (left == 1 || right == 1) {
+            res++;
+            return -1;
+        }
+        
+        else if (left == -1 || right == -1)
+            return 0;
+        
+        else return 1;
+    }
+};
